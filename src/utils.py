f8e7e5c64d59dff1d7deef4ab5d2df22bb0fb337
Max Hasslberger <max.hasslberger@tum.de>
2023-12-04 11:48:23 +0100

    clean up

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,3 @@
-"""
-File:     deep-fus/src/utils.py
-Author:   Tommaso Di Ianni (todiian@stanford.edu)
-
-Copyright 2021 Tommaso Di Ianni
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
 
 # Import packages
 import numpy as np
